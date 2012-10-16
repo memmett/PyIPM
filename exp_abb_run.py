ABB: Fix units.

exp_abb_run.py
@@ -10,12 +10,14 @@ import numpy as np
 from methods import MidPoint
 from kernels.abb import ABBSW, ABBAW
 
+import matplotlib.pylab as plt
+
 
 ###############################################################################
 # config
 
 N = 400
-L = 0.0
+L = 10.0
 U = 300.0
 
 T = range(0, 10)
@@ -49,36 +51,35 @@ logger.addHandler(info)
 
 
 ###############################################################################
-# test
-
-# n = np.zeros((2, len(sw.x)))
-# n[0] = np.exp(-(sw.x - U/2)**2/(U/8)**2)
-# n[1] = 0.5 * np.exp(-(sw.x - U/2)**2/(U/8)**2)
-
-# import matplotlib.pylab as plt
-# plt.plot(sw.x, n[0], '-r')
-# plt.plot(sw.x, n[1], '-b')
-# plt.show()
-
-
-# set current populations and resample
-# for k in [ sw, aw ]:
-#     k.update(n[0], n[1], 0)
-
-# print sw.sw_pct(np.array([100.0, 101.0]))
-
-
-###############################################################################
 # main
 
-logging.info("RUN STARTED AT: %s", time.asctime())
+logging.info("start:  %s", time.asctime())
 logging.info("method: %s", method.name)
 
 n = np.zeros((len(T), 2, len(sw.x)))
 
 # XXX: initial condition
+M = 0.5 * (L + U)
+n[0, 0] = np.exp(-(sw.x - M)**2/(M/4)**2)
+n[0, 1] = 0.5 * np.exp(-(sw.x - M)**2/(M/4)**2)
+
+# for k in [ sw, aw ]:
+#     k.update(n[0, 0], n[0, 1], 0)
+# print sw.sw_pct(np.array([0.0]))
+# print sw.sw_pct(np.array([M]))
+# raise SystemExit
+
+
+
+plt.plot(n[0, 0], '-b')
+plt.plot(n[0, 1], '-r')
+plt.title(str(0))
+plt.figure()
+plt.show()
 
 for j, t in enumerate(T[1:]):
+
+    logging.info('step: %d (%s)', t, time.asctime())
 
     # set current populations and resample
     for k in [ sw, aw ]:
@@ -88,3 +89,8 @@ for j, t in enumerate(T[1:]):
     n[j+1, 0] = sw.project(n[j, 0])
     n[j+1, 1] = aw.project(n[j, 1])
 
+
+    plt.plot(n[j+1, 0], '-b')
+    plt.plot(n[j+1, 1], '-r')
+    plt.title(str(j+1))
+    plt.figure()

kernels/abb.py
@@ -1,9 +1,22 @@
-"""IPM kernels for the Alberta boreal SW and AW species."""
+"""IPM kernels for the Alberta boreal SW and AW species.
+
+Note that throughout these kernels the units are:
+
+  * nsw, naw:    # / ha
+  * dbh (x):     mm
+  * sw_pct:      # / 10m^2
+  * sw_psd:      m / ha
+  * aw_pba:      m^2 / ha
+  * net_pba:     m^2 / ha
+  * di:          cm / y
+  *
+
+"""
 
 import logging
 import numpy as np
 
-from numpy import pi, exp, dot, ones, array
+from numpy import pi, exp, sqrt
 from utils.stats import dnorm
 
 from base import Kernel
@@ -12,48 +25,58 @@ from base import Kernel
 class ABB(Kernel):
 
 
-    def __init__(self):
-        Kernel.__init__(self)
-
-        self.sw_pct  = np.frompyfunc(self._sw_pct,  1, 1)
-        self.sw_psd  = np.frompyfunc(self._sw_psd,  1, 1)
-        self.aw_pba  = np.frompyfunc(self._aw_pba,  1, 1)
-        self.net_pba = np.frompyfunc(self._net_pba, 1, 1)
-
-        
     def update(self, nsw, naw, t, **kwargs):
         self.nsw = nsw
         self.naw = naw
         self.method.sample(self, t)
 
 
-    def _sw_pct(self, x):
+    def sw_pct(self, x):
         """Return number of Spruce with dbh greater than *x*."""
 
-        idx = self.x >= x
-        return dot(self.method.P[idx], self.nsw[idx])
+        r = np.empty(x.shape)
+
+        for i in xrange(len(x)):
+            idx = self.x >= x[i]
+            r[i] = np.dot(self.method.P[idx], self.nsw[idx]) 
+
+        return r / 1e3          # convert # / ha to # / 10 m^2
 
 
-    def _sw_psd(self, x):
+    def sw_psd(self, x):
         """Return Spruce sum-of-diameters for Spruce with dbh greater than *x*."""
 
-        idx = self.x >= x
-        return dot(self.method.P[idx], self.nsw[idx] * self.x[idx])
+        r = np.empty(x.shape)
+
+        for i in xrange(len(x)):
+            idx = self.x >= x[i]
+            r[i] = np.dot(self.method.P[idx], self.nsw[idx] * self.x[idx])
+
+        return r / 1e3          # convert mm / ha to m / ha
 
 
-
-    def _aw_pba(self, x):
+    def aw_pba(self, x):
         """Return Aspen basal area for Aspen with dbh greater than *x*."""
 
-        idx = self.x >= x
-        return dot(self.method.P[idx], self.naw[idx] * pi * (self.x[idx]/2.0)**2)
+        r = np.empty(x.shape)
+
+        for i in xrange(len(x)):
+            idx = self.x >= x[i]
+            r[i] = np.dot(self.method.P[idx], self.naw[idx] * pi * (self.x[idx]/2.0)**2)
+
+        return r / 1e6          # convert mm^2 / ha to m^2 / ha
 
 
-    def _net_pba(self, x):
+    def net_pba(self, x):
         """Return total basal area for trees with dbh greater than *x*."""
 
-        idx = self.x >= x
-        return dot(self.method.P[idx], (self.naw[idx] + self.nsw[idx]) * pi * (self.x[idx]/2.0)**2)
+        r = np.empty(x.shape)
+
+        for i in xrange(len(x)):
+            idx = self.x >= x[i]
+            r[i] = np.dot(self.method.P[idx], (self.naw[idx] + self.nsw[idx]) * pi * (self.x[idx]/2.0)**2)
+
+        return r / 1e6          # convert mm^2 / ha to m^2 / ha
 
 
 
@@ -63,29 +86,32 @@ class ABBSW(ABB):
     def __init__(self):
         ABB.__init__(self)
 
-        self.growth_params = array(
+        self.growth_params = np.array(
             [ 1.358768, 1.025693, -5.700839e-5, -2.256733e-1, -1.882815e-2 ])
 
-        self.survival_params = array(
+        self.survival_params = np.array(
             [ 3.3668568, 0.023043, -0.183418, -0.041333 ])
+
+        self.sd = sqrt(3.687244)
+
 
 
     def kernel(self, x, y, t):
 
-        one    = ones(len(x))
+        one    = np.ones(len(x))
         dbh    = x
         sw_pct = self.sw_pct(x)
         aw_pba = self.aw_pba(x)
 
         # growth
-        xi = array([ one, dbh, dbh**2, sw_pct, aw_pba ])
-        mu = dot(self.growth_params, xi)
+        xi = np.vstack([ one, dbh, dbh**2, sw_pct, aw_pba ])
+        mu = np.dot(self.growth_params, xi)
 
-        g = dnorm(y, mu)
+        g = dnorm(y, mu, sd=self.sd)
 
         # survival
-        xi = array([ one, dbh, sw_pct, aw_pba ])
-        mu = dot(self.survival_params, xi)
+        xi = np.vstack([ one, dbh, sw_pct, aw_pba ])
+        mu = np.dot(self.survival_params, xi)
 
         s = exp(mu) / (1.0 + exp(mu))
 
@@ -98,16 +124,18 @@ class ABBAW(ABB):
     def __init__(self):
         ABB.__init__(self)
 
-        self.growth_params = array(
+        self.growth_params = np.array(
             [ 3.6741201, 0.9954211, -0.0075639, -0.0139588 ])
 
-        self.survival_params = array(
-            [ 2.20, 0.207, -0.00286, -.659, -0.0000778 ])
+        self.survival_params = np.array(
+            [ 2.20, 0.0207, -0.0000286, -0.0659, -0.000000778 ])
+
+        self.sd = sqrt(3.687244)
 
 
     def kernel(self, x, y, t):
 
-        one     = ones(len(x))
+        one     = np.ones(len(x))
         dbh     = x
         di      = y - x
         sw_psd  = self.sw_psd(x)
@@ -115,16 +143,16 @@ class ABBAW(ABB):
         net_pba = self.net_pba(x)
 
         # growth
-        xi = array([ one, dbh, aw_pba, sw_psd ])
-        mu = dot(self.growth_params, xi)
+        xi = np.vstack([ one, dbh, aw_pba, sw_psd ])
+        mu = np.dot(self.growth_params, xi)
 
-        g = dnorm(y, mu)
+        g = dnorm(y, mu, sd=self.sd)
 
         # survival
-        xi = array([ one, dbh**2, di, dbh**2 * net_pba ])
-        mu = dot(self.survival_params, xi)
+        xi = np.vstack([ one, dbh, dbh**2, di, dbh**2 * net_pba ])
+        mu = np.dot(self.survival_params, xi)
 
-        s = exp(mu) / (1.0 + exp(mu))
+        s  = exp(mu) / (1.0 + exp(mu))
 
         return g * s
 